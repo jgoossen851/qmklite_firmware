mjohns/qmk_firmware

Initial arm serial partially based on old lets split code

quantum/stm32/halconf.h
@@ -62,7 +62,7 @@
  * @brief   Enables the EXT subsystem.
  */
 #    if !defined(HAL_USE_EXT) || defined(__DOXYGEN__)
-#        define HAL_USE_EXT FALSE
+#        define HAL_USE_EXT TRUE
 #    endif
 
 /**

jgoossen851/qmklite_firmware

quantum/stm32/halconf.h
@@ -203,7 +203,7 @@
  * @note    Disabling this option saves both code and data space.
  */
 #    if !defined(PAL_USE_CALLBACKS) || defined(__DOXYGEN__)
-#        define PAL_USE_CALLBACKS FALSE
+#        define PAL_USE_CALLBACKS TRUE
 #    endif
 
 /**
@@ -211,7 +211,7 @@
  * @note    Disabling this option saves both code and data space.
  */
 #    if !defined(PAL_USE_WAIT) || defined(__DOXYGEN__)
-#        define PAL_USE_WAIT FALSE
+#        define PAL_USE_WAIT TRUE
 #    endif
 
 /*===========================================================================*/